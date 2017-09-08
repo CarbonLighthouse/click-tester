c8f3a9934422e4606fa081304ed78d0a526b164d
Will Vaughn <willieviseoae@gmail.com>
2017-09-07 17:49:47 -0700

    copy/pasta typo

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author="CarbonLighthouse",
     description=("A unittest.TestCase subclass which provides"
                  "convenience methods for testing click commands."),
-    long_description="See: http://github.com/nackjicholson/ergast-python",
+    long_description="See: http://github.com/CarbonLighthouse/click-tester",
     keywords="click testing unittest TestCase pocoo",
     license='MIT',
     url="http://github.com/CarbonLighthouse/click-tester"
