e1306c5938a6f097b2448c0a0f1abbcf00269301
Will Vaughn <willieviseoae@gmail.com>
2017-09-07 17:47:48 -0700

    setup for pypi publishing

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+rm -rf build dist
+python setup.py sdist
+python setup.py bdist_wheel
+
+# twine upload dist/*
+
--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@ from setuptools import setup, find_packages
 
 setup(
     name='click-tester',
-    version='0.0.0',
-    description='Provides utilities for testing command line tools built with Click.',
-    url='https://github.com/CarbonLighthouse/click-tester',
-    license='MIT',
+    version='0.0.1',
     packages=find_packages(),
     install_requires=[
         'click>=6,<7'
-    ]
+    ],
+    author="CarbonLighthouse",
+    description=("A unittest.TestCase subclass which provides"
+                 "convenience methods for testing click commands."),
+    long_description="See: http://github.com/nackjicholson/ergast-python",
+    keywords="click testing unittest TestCase pocoo",
+    license='MIT',
+    url="http://github.com/CarbonLighthouse/click-tester"
 )
